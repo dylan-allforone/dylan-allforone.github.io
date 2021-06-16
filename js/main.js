e8f5258d4ec502e69a4aaedaf64b25c751cc4ff5
dylan-allforone <1155142316@link.cuhk.edu.hk>
2021-06-16 17:36:26 +0800

    2.0

--- js/main.js
+++ js/main.js
@@ -1,51 +1,57 @@
-/*==================== MENU SHOW Y HIDDEN ====================*/
-const navMenu = document.getElementById('nav-menu'),
-  navToggle = document.getElementById('nav-toggle'),
-  navClose= document.getElementById('nav-close')
 /*===== MENU SHOW =====*/
-/* Validate if constant exists */
-if(navToggle){
-  navToggle.addEventListener('click', () =>{
-    navMenu.classLsit.add('show-menu')
-  })
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
 }
+showMenu('nav-toggle','nav-menu')
 
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-
-
-/*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*==================== ACCORDION SKILLS ====================*/
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
 
+/*SCROLL HOME*/
+sr.reveal('.home__title',{});
+sr.reveal('.button',{delay: 200});
+sr.reveal('.home__img',{delay: 400});
+sr.reveal('.home__social-icon',{ interval: 200});
 
-/*==================== QUALIFICATION TABS ====================*/
+/*SCROLL ABOUT*/
+sr.reveal('.about__img',{});
+sr.reveal('.about__subtitle',{delay: 400});
+sr.reveal('.about__text',{delay: 400});
 
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle',{});
+sr.reveal('.skills__text',{});
+sr.reveal('.skills__data',{interval: 200});
+sr.reveal('.skills__img',{delay: 600});
 
-/*==================== SERVICES MODAL ====================*/
+/*SCROLL WORK*/
+sr.reveal('.work__img',{interval: 200});
 
-
-/*==================== PORTFOLIO SWIPER  ====================*/
-
-
-/*==================== TESTIMONIAL ====================*/
-
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-
-
-/*==================== CHANGE BACKGROUND HEADER ====================*/
-
-
-/*==================== SHOW SCROLL UP ====================*/
-
-
-/*==================== DARK LIGHT THEME ====================*/
+/*SCROLL CONTACT*/
+sr.reveal('.contact__input',{interval: 200});
